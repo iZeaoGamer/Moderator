1fce66da14d12b2df822a325a17a98f8c71f8ada
Driesboy <coenendries3@gmail.com>
2017-04-22 17:19:15 +0200

    Update Main.php

--- src/WarnPlayer/Main.php
+++ src/WarnPlayer/Main.php
@@ -36,74 +36,34 @@
       }
 
       if($file === "2") {
-        $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 1 10 " . $reason);
-        $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 2 10 " . $reason);
-        $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 3 10 " . $reason);
-        $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 4 10 " . $reason);
-        $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 5 10 " . $reason);
-        $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 6 10 " . $reason);
-        $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 7 10 " . $reason);
-        $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 8 10 " . $reason);
-        $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 9 10 " . $reason);
-        $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 10 10 " . $reason);
+        $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"tempban-ip " . $player_name . " 600 " . $reason);
+        $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"tempban " . $player_name . " 600 " . $reason);
 
-        $player->sendMessage(TF::RED . "You are Jailed for " . $reason . " by a Moderator");
-        $player->sendMessage(TF::RED . "10 min left");
-        $sender->sendMessage(TF::GREEN . "" . $player_name . " has been Jailed for 10 min!");
+        $sender->sendMessage(TF::GREEN . "" . $player_name . " has been BANNED for 10 min!");
 
       }
 
       if($file === "3") {
-        $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 1 60 " . $reason);
-        $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 2 60 " . $reason);
-        $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 3 60 " . $reason);
-        $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 4 60 " . $reason);
-        $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 5 60 " . $reason);
-        $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 6 60 " . $reason);
-        $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 7 60 " . $reason);
-        $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 8 60 " . $reason);
-        $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 9 60 " . $reason);
-        $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 10 60 " . $reason);
+        $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"tempban-ip " . $player_name . " 3600 " . $reason);
+        $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"tempban " . $player_name . " 3600 " . $reason);
 
-        $player->sendMessage(TF::RED . "You are Jailed for " . $reason . " by a Moderator");
-        $player->sendMessage(TF::RED . "1 hour left");
-        $sender->sendMessage(TF::GREEN . "" . $player_name . " has been Jailed for 1 hour!");
+        $sender->sendMessage(TF::GREEN . "" . $player_name . " has been BANNED for 1 hour!");
 
       }
 
       if($file === "4") {
-        $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 1 1440 " . $reason);
-        $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 2 1440 " . $reason);
-        $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 3 1440 " . $reason);
-        $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 4 1440 " . $reason);
-        $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 5 1440 " . $reason);
-        $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 6 1440 " . $reason);
-        $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 7 1440 " . $reason);
-        $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 8 1440 " . $reason);
-        $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 9 1440 " . $reason);
-        $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 10 1440 " . $reason);
+        $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"tempban-ip " . $player_name . " 86400 " . $reason);
+        $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"tempban " . $player_name . " 86400 " . $reason);
 
-        $player->sendMessage(TF::RED . "You are Jailed for " . $reason . " by a Moderator");
-        $player->sendMessage(TF::RED . "1 day left");
-        $sender->sendMessage(TF::GREEN . "" . $player_name . " has been Jailed for 1 day!");
+        $sender->sendMessage(TF::GREEN . "" . $player_name . " has been BANNED for 1 day!");
 
       }
 
       if($file >= "5") {
-        $this->getServer()->dispatchCommand(new ConsoleCommandSender(), "jail " . $player_name . " 1 10080 " . $reason);
-        $this->getServer()->dispatchCommand(new ConsoleCommandSender(), "jail " . $player_name . " 2 10080 " . $reason);
-        $this->getServer()->dispatchCommand(new ConsoleCommandSender(), "jail " . $player_name . " 3 10080 " . $reason);
-        $this->getServer()->dispatchCommand(new ConsoleCommandSender(), "jail " . $player_name . " 4 10080 " . $reason);
-        $this->getServer()->dispatchCommand(new ConsoleCommandSender(), "jail " . $player_name . " 5 10080 " . $reason);
-        $this->getServer()->dispatchCommand(new ConsoleCommandSender(), "jail " . $player_name . " 6 10080 " . $reason);
-        $this->getServer()->dispatchCommand(new ConsoleCommandSender(), "jail " . $player_name . " 7 10080 " . $reason);
-        $this->getServer()->dispatchCommand(new ConsoleCommandSender(), "jail " . $player_name . " 8 10080 " . $reason);
-        $this->getServer()->dispatchCommand(new ConsoleCommandSender(), "jail " . $player_name . " 9 10080 " . $reason);
-        $this->getServer()->dispatchCommand(new ConsoleCommandSender(), "jail " . $player_name . " 10 10080 " . $reason);
+        $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"tempban-ip " . $player_name . " 604800 " . $reason);
+        $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"tempban " . $player_name . " 604800 " . $reason);
 
-        $player->sendMessage(TF::RED . "You are Jailed for " . $reason . " by a Moderator");
-        $player->sendMessage(TF::RED . "1 week left");
-        $sender->sendMessage(TF::GREEN . "" . $player_name . " has been Jailed for 1 week!");
+        $sender->sendMessage(TF::GREEN . "" . $player_name . " has been BANNED for 1 week!");
 
       }else{
         $player->kick(TextFormat::YELLOW . "You are warned for " . $reason . " by a Moderator", false);
