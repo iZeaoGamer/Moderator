1bb166a99afe1218b4b3b4a85f874f10fac27d00
Driesboy <coenendries3@gmail.com>
2017-06-25 08:54:43 +0200

    Bug fixes

--- src/Driesboy/Main.php
+++ src/Driesboy/Main.php
@@ -31,13 +31,14 @@ class Main extends PluginBase implements Listener {
   public function warn(Player $player, Player $sender , $reason){
 
     $player_name = $player->getName();
-    $file = file_get_contents($this->getDataFolder() . "Players/" . strtolower($player_name) . ".txt");
-    file_put_contents($this->getDataFolder() . "Players/" . strtolower($player_name) . ".txt", $file + 1);
 
     if(!(file_exists($this->getDataFolder() . "Players/" . strtolower($player_name) . ".txt"))) {
       touch($this->getDataFolder() . "Players/" . strtolower($player_name) . ".txt");
-      file_put_contents($this->getDataFolder() . "Players/" . strtolower($player_name) . ".txt", "0");
+      file_put_contents($this->getDataFolder() . "Players/" . strtolower($player_name) . ".txt", "1");
+    }else{
+      file_put_contents($this->getDataFolder() . "Players/" . strtolower($player_name) . ".txt", $file + 1);
     }
+    $file = file_get_contents($this->getDataFolder() . "Players/" . strtolower($player_name) . ".txt");
 
     if($file === "2") {
       $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"nban " . $player_name . " 1hour " . $reason);
